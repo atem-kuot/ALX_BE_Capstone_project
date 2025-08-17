37d9c23a2e5037f6561952d56ac947022d876ef5
atem-kuot <atemliaikuot@gmail.com>
2025-08-17 08:42:53 +0200

    add alerts app

--- Pharmacy_Inventory_API/Pharmacy_Inventory_API/settings.py
+++ Pharmacy_Inventory_API/Pharmacy_Inventory_API/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core'
-    'medicines'
-    'prescriptions'
+    'core',
+    'medicines',
+    'prescriptions',
+    'alerts',
 ]
 
 MIDDLEWARE = [
