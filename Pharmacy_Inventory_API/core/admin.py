1e387ea035f072c24730fe68a1d63fe08ab53afa
atem-kuot <atemliaikuot@gmail.com>
2025-08-24 12:27:08 +0200

    Add core\admin.py

--- Pharmacy_Inventory_API/core/admin.py
+++ Pharmacy_Inventory_API/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'phone', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Pharmacy Role', {'fields': ('role', 'phone')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Pharmacy Role', {'fields': ('role', 'phone')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
 # Register your models here.
