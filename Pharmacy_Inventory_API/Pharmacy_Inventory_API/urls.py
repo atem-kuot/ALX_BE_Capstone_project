9d362e217bd2877164e60af235f6d87cbb6a6b08
atem-kuot <atemliaikuot@gmail.com>
2025-08-31 21:00:43 +0200

    update Phar....urls.py

--- Pharmacy_Inventory_API/Pharmacy_Inventory_API/urls.py
+++ Pharmacy_Inventory_API/Pharmacy_Inventory_API/urls.py
@@ -16,10 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("""
+    <h1>🏥 Pharmacy Management API</h1>
+    <p>API is running successfully!</p>
+    <ul>
+        <li><a href="/api/auth/login/">Login</a></li>
+        <li><a href="/admin/">Admin</a></li>
+        <li><a href="/api/medicines/">Medicines API</a></li>
+    </ul>
+    """)
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('api/core/', include('core.urls')),
+    path('api/auth/', include('core.urls')),
     path('api/medicines/', include('medicines.urls')),
     path('api/prescriptions/', include('prescriptions.urls')),
     path('api/alerts/', include('alerts.urls')),
