2c17f2042e0811415e411705017746a493f88307
atem-kuot <atemliaikuot@gmail.com>
2025-08-31 18:23:19 +0200

    update production_settings

--- Pharmacy_Inventory_API/Pharmacy_Inventory_API/settings.py
+++ Pharmacy_Inventory_API/Pharmacy_Inventory_API/settings.py
@@ -92,8 +92,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Pharmacy_Inventory_API.wsgi.application'
 
-
-
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
