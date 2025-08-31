fdfce9834e9e5606105ea7564c2e6496a2193746
atem-kuot <atemliaikuot@gmail.com>
2025-08-31 17:43:03 +0200

    Add production_settings

--- Pharmacy_Inventory_API/Pharmacy_Inventory_API/production_settings.py
+++ Pharmacy_Inventory_API/Pharmacy_Inventory_API/production_settings.py
@@ -4,27 +4,32 @@ from .settings import *
 DEBUG = False
 ALLOWED_HOSTS = ['AtemKuot.pythonanywhere.com', 'localhost']
 
-# PostgreSQL database
+ # MySQL database configuration for PythonAnywhere
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'pharmacy_db'),
-        'USER': os.getenv('DB_USER', 'pharmacy_user'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'yourusername$pharmacy_db'),
+        'USER': os.getenv('DB_USER', 'yourusername'),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('DB_HOST', 'yourusername.mysql.pythonanywhere-services.com'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
 # Static files
-STATIC_ROOT = '/home/AtemKuot/Pharmacy_Inventory_API/static'
+STATIC_ROOT = '/home/yourusername/pharmacy_api/static'
 STATIC_URL = '/static/'
-
-# Media files
-MEDIA_ROOT = '/home/AtemKuot/Pharmacy_Inventory_API/media'
-MEDIA_URL = '/media/'
 
 # Security settings
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# For MySQL compatibility
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
