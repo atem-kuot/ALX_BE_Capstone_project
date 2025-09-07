0511a2200518bac0be4ff6a03c0d4284b6612570
atem-kuot <atemliaikuot@gmail.com>
2025-09-07 23:07:17 +0200

    update core/serializers.py

--- Pharmacy_Inventory_API/core/serializers.py
+++ Pharmacy_Inventory_API/core/serializers.py
@@ -5,21 +5,21 @@ from .models import User
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
-    confirm_password = serializers.CharField(write_only=True)  
-    
+    confirm_password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password', 'confirm_password', 'role', 'phone', 'first_name', 'last_name')
         extra_kwargs = {
             'password': {'write_only': True},
-            'role': {'required': True}
+            'role': {'required': True},
+            'phone': {'required': False, 'allow_blank': True}  # Make phone optional
         }
-        
     def validate(self, data):
         if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("Passwords do not match.")
         return data
-        
+
     def create(self, validated_data):
         validated_data.pop('confirm_password')
         password = validated_data.pop('password')
@@ -31,11 +31,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
-    
+
     def validate(self, data):
         username = data.get('username')
         password = data.get('password')
-        
+
         if username and password:
             user = authenticate(username=username, password=password)
             if user:
